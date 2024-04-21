fe6ba1eea7adc6f5df2cfe5b37e2c32a38260db0
karlfranz <2031665973@qq.com>
2024-04-21 17:44:45 +0800

    修复报错

--- dist/hooks/useFuncDebounce.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const useFuncDebounce: () => <T extends Function, K, U extends unknown[]>(callback: T, delay?: number, change?: boolean) => (object?: K | undefined, ...args: U) => void;
